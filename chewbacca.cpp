b76b423cc77c006b28119352502b69fa9dc7c2a3
SHIVAM KUMAR SWARNKAR <sswarnkar0001@gmail.com>
2025-01-18 23:46:59 +0530

    chewbacca.cpp

--- chewbacca.cpp
+++ chewbacca.cpp
@@ -1,1 +1,31 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    string s;
+    cin >> s;
+    
+    string res = "";
+    
+    for(int i=0;i<s.size();i++){
+        char ch = s[i];
+        int a = ch - '0';
+        
+        if(i==0 && (a==9)){
+            res += '0'+a;
+            continue;  
+        }
+        
+        if(a>=5){
+            res += '0' + (9-a);
+        }
+        
+        else{
+            res += ch;
+        }
+    }
+    
+    cout << res << endl;
+    
+    return 0;
+}
